e3eb61714262d585d52013550b94ac8938b1f51d
mheshan27 <mheshan27@gmail.com>
2021-07-15 14:45:17 +0530

    add image

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ $('.sidenav').sidenav();
 
 //animate acording to scroll
 var btn = document.querySelector(".animbtn");
+var keyframe = find
 $(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
            
-        btn.style.display="none";
-                } else {
-                    btn.style.display="";
+    
+            btn.style.animationName="moveDown";
+            btn.style.opacity="0";
+           
+         } else {
+                    btn.style.animationName="moveInBottom";
+    
+                       
+                        btn.style.opacity="1";
+           
     }
 });
+
+
+
+
+
 
 $(document).ready(function(){
 
